febac3e4039a717509133040f68f62bd5a85d925
Александр Шешин <sheshin.alexanderkpc2052020@gmail.com>
2022-11-22 20:01:11 +0300

    remove prettier

--- init.lua
+++ init.lua
@@ -259,7 +259,7 @@ local config = {
                         -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
                         config.sources = {
                                 -- Set a formatter
-                                null_ls.builtins.formatting.prettier,
+                                -- null_ls.builtins.formatting.prettier,
                                 null_ls.builtins.formatting.autopep8,
                                 null_ls.builtins.diagnostics.flake8,
                                 null_ls.builtins.diagnostics.eslint,
